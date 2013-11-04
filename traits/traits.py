91125911473f1b70db11553f85d9ca15971ad1fe
Justin Smith <jtsmith1287@gmail.com>
2013-11-04 08:48:26 -0800

    repo sync

---
+++ traits/traits.py
@@ -0,0 +1,33 @@
+
+import os.path
+
+files = ["skills.gdat", "advantages.gdat", "disadvantages.gdat"]
+data = {}
+
+for gdat in files:
+  file_path = os.path.join(os.path.dirname(__file__), gdat)
+  with open(file_path, "r") as f:
+    data[gdat] = f.readlines()
+
+SKILL_CATEGORIES = set([])
+SKILLS = []
+ADVANTAGES_LIST = []
+DISADVANTAGES_LIST = []
+
+for line in data["skills.gdat"]:
+  item = eval(line)
+  SKILLS.append(item)
+  for cat in item[-1]:
+    SKILL_CATEGORIES.add(cat)
+for line in data["advantages.gdat"]:
+  print line
+  item = eval(line)
+  ADVANTAGES_LIST.append(item)
+for line in data["disadvantages.gdat"]:
+  item = eval(line)
+  DISADVANTAGES_LIST.append(item)
+
+print SKILL_CATEGORIES
+print SKILLS
+print ADVANTAGES_LIST
+print DISADVANTAGES_LIST
